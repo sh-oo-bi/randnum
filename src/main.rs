aedfd7d8765cb004940fb747a7f17f66bf1a3165
sh-oo-bi <abd.shobi@gmail.com>
2023-06-14 05:27:10 +0000

    start using warp

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
-fn main() {
-    println!("Hello, world!");
+use warp::Filter;
+
+#[tokio::main]
+async fn main() {
+    let route = warp::path("randnum").map(|| format!("123456"));
+
+    warp::serve(route).run(([127, 0, 0, 1], 1337)).await;
 }
