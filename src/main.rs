ba0806ed0fa9f3a8f84b8510d0afc7b3fc5c2973
sh-oo-bi <abd.shobi@gmail.com>
2023-06-24 13:58:18 +0000

    now user can choose Range: e.g.  .../randnum?start=1&end=20

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use rand::Rng;
 use serde::Serialize;
+use std::collections::HashMap;
 use warp::{Filter, Rejection};
 
 #[derive(Debug, Serialize)]
@@ -8,18 +9,40 @@ struct Randnum {
 }
 
 impl Randnum {
-    fn new() -> Self {
+    fn new(start: Option<i32>, end: Option<i32>) -> Self {
+        let start = match start {
+            Some(n) => n,
+            _ => 1,
+        };
+
+        let end = match end {
+            Some(n) => n,
+            _ => 1000,
+        };
+
         let random = {
             let mut rng = rand::thread_rng();
-            rng.gen_range(1..=1000)
+            rng.gen_range(start..=end)
         };
 
         Self { num: random }
     }
 }
 
-async fn get_random() -> Result<impl warp::Reply, Rejection> {
-    let random_number = Randnum::new();
+async fn get_random(params: HashMap<String, String>) -> Result<impl warp::Reply, Rejection> {
+    let start = match params.get("start") {
+        Some(n) => Some(n.parse::<i32>().expect("Could not parse start")),
+        None => None,
+    };
+
+    let end = match params.get("end") {
+        Some(n) => Some(n.parse::<i32>().expect("Could not parse end")),
+        None => None,
+    };
+
+    println!("start:{:?}, end:{:?}", start, end);
+
+    let random_number = Randnum::new(start, end);
 
     Ok(warp::reply::json(&random_number))
 }
@@ -29,6 +52,7 @@ async fn main() {
     let get_random = warp::get()
         .and(warp::path("randnum"))
         .and(warp::path::end())
+        .and(warp::query())
         .and_then(get_random);
 
     let route = get_random;
